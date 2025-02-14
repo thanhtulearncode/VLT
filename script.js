ce5e404a8c7a04082aa6d9a54b342d431245d6f2
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2025-02-14 22:07:29 +0700

    Update 1.0.1

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.getElementById("yes").addEventListener("click", function() {
     window.location.href = "yes.html";
 });
 
-document.getElementById("no").addEventListener("mouseover", function() {
+document.getElementById("no").addEventListener("click", function() {
+    moveNo();
+});
+
+function moveNo() {
     let btn = document.getElementById("no");
     let x = Math.random() * window.innerWidth * 0.7;
     let y = Math.random() * window.innerHeight * 0.7;
     btn.style.position = "absolute";
     btn.style.left = x + "px";
     btn.style.top = y + "px";
-});
-
+}
