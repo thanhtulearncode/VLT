ca898009414fa7919e8c28b4811a45eee97e10d9
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2025-02-14 22:03:50 +0700

    Version 1.0

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.getElementById("yes").addEventListener("click", function() {
+    window.location.href = "yes.html";
+});
+
+document.getElementById("no").addEventListener("mouseover", function() {
+    let btn = document.getElementById("no");
+    let x = Math.random() * window.innerWidth * 0.7;
+    let y = Math.random() * window.innerHeight * 0.7;
+    btn.style.position = "absolute";
+    btn.style.left = x + "px";
+    btn.style.top = y + "px";
+});
 
